2fc31b945c6c9919699f017afc9dcc31cf9d73f4
Microtribute <46481195+Microtribute@users.noreply.github.com>
2023-01-13 05:01:26 +0800

    update

--- natural-scrolling.ps1
+++ natural-scrolling.ps1
@@ -1,73 +1,97 @@
-# Enable natural scrolling for all configured USB mice on the system - pulling
-# down the mouse wheel pulls the screen area down, showing the text above.
+# Enable natural scrolling for all configured USB Mice on the system - pulling
+# down the Mouse wheel pulls the screen area down, showing the text above.
 #
 # Use the "reverse" argument to enable unnatural a.k.a. Microsoft default
-# scrolling - pulling down the mouse wheel shows the text below.
+# scrolling - pulling down the Mouse wheel shows the text below.
 #
 # Run this script in PowerShell as administrator.
 #
-# The mouse behavior changes only after rebooting the system.
+# The Mouse behavior changes only after rebooting the system.
+
+
+# Function to modify registry values
+function UpdateRegistryParameter {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$InstanceId,
+
+        [Parameter(Mandatory = $true)]
+        [string]$ParameterName,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateRange(0, 1)]
+        [int]$TargetValue
+    )
+
+    $RegistryPath = "HKLM:\SYSTEM\CurrentControlSet\Enum\$InstanceId\Device Parameters"
+
+    $CurrentValue = (Get-ItemProperty -Path $RegistryPath -Name $ParameterName).$ParameterName
+
+    $ValueUpdated = $false;
+
+    if ($CurrentValue -ne $TargetValue) {
+        Set-ItemProperty -Path $RegistryPath -Name $ParameterName -Value $TargetValue
+        $ValueUpdated = $true;
+    }
+
+    return $ValueUpdated
+}
+
 
 # Check if we have admin rights
-$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$CurrentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
         
-if (!$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-     Write-Host -ForegroundColor Red "Need admin privilege to modify the registry"
+if (!$CurrentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+     Write-Host -ForegroundColor Red "Admin privilege required to modify the registry as necessary"
      exit 1
 }
 
-$scrollingBehaviors = @("DEFAULT", "NATURAL")
+$ScrollingBehaviors = @("DEFAULT", "NATURAL")
 
-$switchValue = $args[0];
+$SwitchValue = $args[0];
 
-if ($switchValue -eq "disable" || $switchValue -eq "reverse") {
-    $targetSettingValue = 0
+if (($SwitchValue -eq "disable") -or ($SwitchValue -eq "reverse")) {
+    $TargetSettingValue = 0
 } else {
-    $targetSettingValue = 1;
+    $TargetSettingValue = 1;
 }
 
-$mice = (Get-PnpDevice -Class Mouse).where{($_.Status -eq "OK") -and ($_.InstanceID -like "HID\VID*")}
+$Mice = (Get-PnpDevice -Class Mouse).where{($_.Status -eq "OK") -and ($_.InstanceID -like "HID\VID*")}
 
-if ($mice.Count -eq 0) {
-    Write-Host -ForegroundColor Red "Could not find any USB mouse. Exiting."
+if ($Mice.Count -eq 0) {
+    Write-Host -ForegroundColor Red "Could not find any USB Mouse. Exiting."
     exit 1
 }
 
-$settingUpdated = $false
+$SettingUpdated = $false
 
-# set up every mouse
-foreach ($mouse in $mice) {
-    $deviceName = $mouse.FriendlyName
-    $deviceInstanceId = $mouse.InstanceID
+foreach ($Mouse in $Mice) {
+    $DeviceName = $Mouse.FriendlyName
+    $DeviceInstanceId = $Mouse.InstanceID
 
-    Write-Host -ForegroundColor White -BackgroundColor DarkGreen "Found a mouse device $deviceName ($deviceInstanceId)"
+    Write-Host -ForegroundColor White -BackgroundColor DarkMagenta "Found a Mouse device $DeviceName ($DeviceInstanceId)"
 
-    Write-Host -ForegroundColor Cyan "Modifying scrolling behaviors"
+    $VerticalScrollingBehaviorUpdated = UpdateRegistryParameter -InstanceId $DeviceInstanceId -ParameterName FlipFlopWheel -TargetValue $TargetSettingValue
 
-    $registryPath = "HKLM:\SYSTEM\CurrentControlSet\Enum\$deviceInstanceId\Device Parameters"
-
-    $currentVerticalScrollingValue = (Get-ItemProperty -Path $registryPath -Name FlipFlopWheel).FlipFlopWheel
-
-    if ($currentVerticalScrollingValue -ne $targetSettingValue) {
-        Set-ItemProperty -Path $registryPath -Name FlipFlopWheel -Value $targetSettingValue
-        Write-Host -ForegroundColor Green "Vertical scrolling behavior has been set to " $scrollingBehaviors[$targetSettingValue]
-        $settingUpdated = $true
+    if ($VerticalScrollingBehaviorUpdated) {
+        Write-Host -ForegroundColor Green "Vertical scrolling behavior has been set to " $ScrollingBehaviors[$TargetSettingValue]
     } else {
-        Write-Host -ForegroundColor Green "Keeping the current vertical scrolling behavior: " $scrollingBehaviors[$targetSettingValue]
+        Write-Host -ForegroundColor Green "Keeping the current vertical scrolling behavior: " $ScrollingBehaviors[$TargetSettingValue]
     }
 
-    $currentHorizontalScrollingValue = (Get-ItemProperty -Path $registryPath -Name FlipFlopHScroll).FlipFlopHScroll
+    $HorizontalScrollingBehaviorUpdated = UpdateRegistryParameter -InstanceId $DeviceInstanceId -ParameterName FlipFlopHScroll -TargetValue $TargetSettingValue
 
-    if ($currentVerticalScrollingValue -ne $targetSettingValue) {
-        Set-ItemProperty -Path $registryPath -Name FlipFlopHScroll -Value $targetSettingValue
-        Write-Host -ForegroundColor Green "Horizontal scrolling behavior has been set to " $scrollingBehaviors[$targetSettingValue]
-        $settingUpdated = $true
+    if ($HorizontalScrollingBehaviorUpdated) {
+        Write-Host -ForegroundColor Green "Horizontal scrolling behavior has been set to " $ScrollingBehaviors[$TargetSettingValue]
     } else {
-        Write-Host -ForegroundColor Green "Keeping the current horizontal scrolling behavior: " $scrollingBehaviors[$targetSettingValue]
+        Write-Host -ForegroundColor Green "Keeping the current horizontal scrolling behavior: " $ScrollingBehaviors[$TargetSettingValue]
     }
+
+    $SettingUpdated = $SettingUpdated -or $VerticalScrollingBehaviorUpdated -or $HorizontalScrollingBehaviorUpdated
 }
 
-if ($settingUpdated) {
+if ($SettingUpdated) {
     Write-Host -ForegroundColor Red "Done. You must reboot your computer for the changes to take effect."
 } else {
     Write-Host -ForegroundColor Red "Done. No changes have been made."
